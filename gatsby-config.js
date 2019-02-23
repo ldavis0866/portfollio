22f76625ab5a388a63a98d97e3004b1a951f4d81
Lonnie Davis <ldavis0866@gmail.com>
2019-02-23 11:48:50 -0600

    change meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
-  },
+     siteMetadata: {
+       title: "Lonnie Davis - Portfolio",
+  //   author: "Hunter Chang",
+  //   description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+     },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
